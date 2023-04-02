8a9a0756065aff235374464f0dd743cbc7ee1121
EwertonCXavier <ewertoncassianox@gmail.com>
2023-04-02 15:44:41 -0300

    refactor: update vite logo folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/starting-react",
+  base: "/starting-react/",
   build: {
     chunkSizeWarningLimit: 1600
   }
